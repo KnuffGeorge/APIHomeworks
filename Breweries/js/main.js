7306fbffbba0164e461cbc7c2b87daf4bd845765
KnuffGeorge <georgethek@gmail.com>
2022-04-13 15:00:20 -0700

    brewery app is dope

--- Breweries/js/main.js
+++ Breweries/js/main.js
@@ -1,25 +1,24 @@
+document.querySelector('button').addEventListener("click", getFetch)
+
 function getFetch(){
-    let inputVal = Vancouver
-    }
-    const url = `https://api.openbrewerydb.org/breweries?by_city=vancouver&per_page=50`
-    let breweryNumber = Math.random() * 20
+    let inputVal = document.querySelector('input').value
+    const url = `https://api.openbrewerydb.org/breweries?by_city=${inputVal}&per_page=50`
     fetch(url)
         .then(res => res.json()) // parse response as JSON
         .then(data => {
-            for (let i = 0; i < data.length; i++)
-            {console.log(data[i])}
+            console.log(data)
+            if(data.length == 0){
+                document.querySelector('.breweryName').innerText = "No breweries found in your city :("   
+            }else{
+            let breweryNumber = Math.floor(Math.random() * data.length)    
+            document.querySelector('.breweryName').innerText = data[breweryNumber].name
+            document.querySelector('.breweryAddress').innerText = data[breweryNumber].street
+            document.querySelector('a.breweryWebsite').href = data[breweryNumber].website_url
+            document.querySelector('a.breweryLocation').href = `https://www.google.com/maps/search/?api=1&query=${data[breweryNumber].latitude}%2C${data[breweryNumber].longitude}&query=${data[breweryNumber].name}`
+            }
         })
         .catch(err => {
             console.log(`error ${err}`)
         })
-
+}
         
-
-        class BreweryInfo {
-            constructor(productData) {
-                this.brand = productData.brands
-                this.name = productData.product_name
-                this.ingredients = productData.ingredients
-                this.image = productData.image_url
-            }
-        }
