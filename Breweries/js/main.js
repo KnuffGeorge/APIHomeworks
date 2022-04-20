62e629cd746dfb40a0613d52ecfa9cbdb5d69bea
KnuffGeorge <georgethek@gmail.com>
2022-04-20 13:42:18 -0700

    NEW BREWERY API (no key)

--- Breweries/js/main.js
+++ Breweries/js/main.js
@@ -1,8 +1,11 @@
+//TINDER FOR BEER
+
 document.querySelector('button').addEventListener("click", getFetch)
 
 function getFetch(){
-    let inputVal = document.querySelector('input').value
-    const url = `https://api.openbrewerydb.org/breweries?by_city=${inputVal}&per_page=50`
+    let city = document.querySelector('.city').value
+    let prov = document.querySelector('.prov').value
+    const url = `http://beermapping.com/webservice/loccity/APIKEY/${city},${prov}&s=json`
     fetch(url)
         .then(res => res.json()) // parse response as JSON
         .then(data => {
@@ -13,12 +16,16 @@ function getFetch(){
             let breweryNumber = Math.floor(Math.random() * data.length)    
             document.querySelector('.breweryName').innerText = data[breweryNumber].name
             document.querySelector('.breweryAddress').innerText = data[breweryNumber].street
-            document.querySelector('a.breweryWebsite').href = data[breweryNumber].website_url
+            document.querySelector('a.breweryWebsite').href = `http://${data[breweryNumber].url}`
             document.querySelector('a.breweryLocation').href = `https://www.google.com/maps/search/?api=1&query=${data[breweryNumber].latitude}%2C${data[breweryNumber].longitude}&query=${data[breweryNumber].name}`
-            }
-        })
+           
+            
+            
+        }  
+        )
         .catch(err => {
             console.log(`error ${err}`)
         })
-}
+    }
+
         
